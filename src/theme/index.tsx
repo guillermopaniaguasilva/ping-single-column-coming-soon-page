7c86bac8e69622a875dcc8907c394844a3c27a8c
Guille <guillermo.paniaguasilva@MacBookPro.local>
2023-06-19 10:12:22 -0300

    finish project setup; add theme

---
+++ src/theme/index.tsx
@@ -0,0 +1,45 @@
+import { DefaultTheme } from 'styled-components';
+import uiTheme, {
+	breakpoints,
+	colors,
+	fontFamilies,
+	fontWeights,
+} from './uiTheme';
+
+declare module 'styled-components' {
+	export interface DefaultTheme {
+		ui: typeof uiTheme;
+		breakpoints: {
+			mobile: string;
+			desktop: string;
+		};
+		colors: {
+			white: string;
+			blue: string;
+			paleBlue: string;
+			lightRed: string;
+			gray: string;
+			veryDarkBlue: string;
+		};
+		fontFamilies: {
+			normal: string;
+		};
+		fontWeights: {
+			light: number;
+			semi: number;
+			bold: number;
+		};
+	}
+}
+
+const ui = uiTheme;
+
+const theme: DefaultTheme = {
+	ui,
+	breakpoints,
+	colors,
+	fontFamilies,
+	fontWeights,
+};
+
+export default theme;
---
+++ src/theme/uiTheme.tsx
@@ -0,0 +1,32 @@
+export const breakpoints = {
+	mobile: '375px',
+	desktop: '1440px',
+};
+
+export const colors = {
+	white: '#fff',
+	blue: 'hsl(223, 87%, 63%)',
+	paleBlue: 'hsl(223, 100%, 88%)',
+	lightRed: 'hsl(354, 100%, 66%)',
+	gray: 'hsl(0, 0%, 59%)',
+	veryDarkBlue: 'hsl(209, 33%, 12%)',
+};
+
+export const fontFamilies = {
+	normal: 'Libre Franklin',
+};
+
+export const fontWeights = {
+	light: 300,
+	semi: 600,
+	bold: 700,
+};
+
+const uiTheme = {
+	breakpoints,
+	colors,
+	fontFamilies,
+	fontWeights,
+};
+
+export default uiTheme;
